2571d2590a04e2511d3d1be5622e080a8533538d
Nathan Da Costa <nathancosta73@outlook.com>
2023-11-26 18:49:54 -0300

    teste carrinho

--- src/js/produtos.js
+++ src/js/produtos.js
@@ -176,8 +176,3 @@ function filtrarProdutos() {
         }
     }
 }
-
-function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-}
